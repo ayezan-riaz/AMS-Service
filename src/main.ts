ee9b72d6146243d1145194621b36599fe575d32c
Syed Saad Luqman <gosaad@outlook.com>
2023-06-25 19:35:16 +0500

    open static file serve

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,12 @@ import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { useContainer } from 'class-validator';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule); //for static image serve
+  //const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
   // {
@@ -27,6 +30,14 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
   /* for Swagger */
+
+  app.useStaticAssets(join(__dirname, '..', '..', 'files')); //for static image serve
+  //for static file service
+  //http://localhost:3000/alumni/default/avatar.jpg
+  //http://localhost:3000/alumni/39/avatar/profilePic.png
+  //http://localhost:3000/alumni/39/profileResume/17.pdf
+  //http://localhost:3000/alumni/39/skillCertificates/6.png
+
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
