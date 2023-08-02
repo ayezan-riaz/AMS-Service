3e3668d066a56dd2466ec4331b3f25e15806cd4f
Syed Saad Luqman <gosaad@outlook.com>
2023-08-02 14:33:30 +0500

    UserUpdateDto

--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -23,30 +23,23 @@ export class UpdateUserDto extends PartialType(CreateUserDto) {
     description: 'Phone Number',
     example: '+92 337033321',
   })
-  @IsNotEmpty()
   phone: string;
 
   @ApiProperty({ description: 'Registration Status', default: 0 })
-  @IsNotEmpty()
   registration_status: number;
 
   @ApiProperty({ description: 'Active Status', default: false })
-  @IsNotEmpty()
   active_status: boolean;
 
   @ApiProperty({ description: 'Password', example: '12345678' })
-  @IsNotEmpty()
   password: string;
 
   @ApiProperty({ description: 'Password Reset Token', example: '12345678' })
-  @IsNotEmpty()
   password_reset_token: string;
 
   @ApiProperty({ description: 'first name', example: 'Syed' })
-  @IsNotEmpty()
   first_name: string;
   @ApiProperty({ description: 'middle name', example: 'Saad' })
-  @IsNotEmpty()
   middle_name: string;
   @ApiProperty({ description: 'last name', example: 'Luqman', required: false })
   last_name: string;
